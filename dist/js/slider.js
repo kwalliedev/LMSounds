856fa99e364b6bc1e675c9578d8d659b657cd7e9
Michiel-VE <r0794174@student.thomasmore.be>
2021-06-25 14:49:01 +0200

    pos in orde

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,11 +1,15 @@
 new Splide('.splide', {
-    height : '30rem',
-    width: "60rem",
+    height : '40.501rem',
+    width: "72rem",
     cover: true,
     lazyLoad: 'sequential',
     type  : 'fade',
     rewind: true,
     breakpoints: {
+        // '2000':{
+        //     height : '19.6rem',
+        //     width: "35rem",
+        // },
         '640': {
             height : '19.6rem',
             width: "35rem",
