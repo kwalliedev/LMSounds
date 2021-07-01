1dd60fc864c47d0982b4ddcd9c8e5fde0e5807a5
Maxim-Snoeys <r0792870@student.thomasmore.be>
2021-07-01 13:14:02 +0200

    kleurtjes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
             },
             animation: {
                 'bounce': 'bounce 2s infinite',
+            },
+            colors: {
+                gray: {
+                    250: 'rgba(221,221,221,0.92)'
+                }
             }
         },
     },
