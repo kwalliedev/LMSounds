861f2fa69280d6e9b4385633a1f51cd43570c736
Michiel-VE <r0794174@student.thomasmore.be>
2021-10-16 11:56:47 +0300

    update images

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,30 +1,3 @@
-//video carrousel
-new Splide('#video', {
-    height: '42.75rem',
-    width: "85rem",
-    cover: true,
-    lazyLoad: 'sequential',
-    type: 'fade',
-    rewind: true,
-    breakpoints: {
-        '1024': {
-            height: '42.751rem',
-            width: "76rem",
-        },
-        '640': {
-            height: '19.6rem',
-            width: "35rem",
-        },
-        '480': {
-            height: '14rem',
-            width: "25rem",
-        },
-    },
-    video: {
-        autoplay: false
-    },
-}).mount(window.splide.Extensions);
-
 new Splide('#galerijFoto', {
     type: 'loop',
     perPage: 2,
