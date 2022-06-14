ca1e25e236fb09251514c41f6bd88f195f31774e
Maxim-Snoeys <r0792870@student.thomasmore.be>
2022-06-14 16:50:40 +0200

    deleted picture + picture data

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -2,14 +2,13 @@ const modal_overlay = document.querySelector('#modal_overlay');
 const modal = document.querySelector('#modal');
 
 //toggle modal
-function openModal(value, src, alt, tekst) {
+function openModal(value, src, alt) {
     const modalCl = modal.classList
     const overlayCl = modal_overlay
 
     if (value && src !== undefined) {
         document.getElementById("image").setAttribute("src", src);
         document.getElementById("image").setAttribute("alt", alt);
-        document.getElementById("tekst").innerHTML = tekst;
         overlayCl.classList.remove('hidden');
         setTimeout(() => {
             modalCl.remove('opacity-0');
