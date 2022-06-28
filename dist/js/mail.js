71771a3113b24ca633ffa523f8e897d401cca9b8
Michiel-VE <r0794174@student.thomasmore.be>
2022-06-28 10:37:36 +0200

    var to let

--- dist/js/mail.js
+++ dist/js/mail.js
@@ -4,22 +4,22 @@
 
 document.getElementById("submitForm").addEventListener('click', function (e) {
     e.preventDefault()
-    var name = document.getElementById('naam');
-    var email = document.getElementById('email');
-    var selection = document.getElementById('onderwerp');
-    var message = document.getElementById('bericht');
+    let name = document.getElementById('naam');
+    let email = document.getElementById('email');
+    let selection = document.getElementById('onderwerp');
+    let message = document.getElementById('bericht');
 
     name.classList.replace("border-red-900", "border-gray-200");
     email.classList.replace("border-red-900", "border-gray-200");
     message.classList.replace("border-red-900", "border-gray-200");
 
-    var errorNaam = naam.nextElementSibling;
+    let errorNaam = naam.nextElementSibling;
     errorNaam.classList.add("hidden");
 
-    var errorMail = email.nextElementSibling;
+    let errorMail = email.nextElementSibling;
     errorMail.classList.add("hidden");
 
-    var errorMsg = message.nextElementSibling;
+    let errorMsg = message.nextElementSibling;
     errorMsg.classList.add("hidden");
 
     function validateEmail(email) {
@@ -31,19 +31,19 @@ document.getElementById("submitForm").addEventListener('click', function (e) {
 
         if (name.value !== "" && message.value !== "" && name.value !== undefined && message.value !== undefined) {
 
-            var templateMail = {
+            let templateMail = {
                 sender_name: name.value,
                 sender_mail: email.value,
                 dropdown_select: selection.value,
                 msg: message.value,
             }
-            var mailModel = document.getElementById("modalmail");
+            let mailModel = document.getElementById("modalmail");
             mailModel.classList.replace('translate-x-full', 'translate-x-0')
             setTimeout(function showModel() {
                 mailModel.classList.replace('translate-x-0', "translate-x-full")
             }, 3000)
 
-            emailjs.send("service_8yacc89", "template_a39y0cg", templateMail)
+            emailjs.send("service_nqwfamb", "template_a39y0cg", templateMail)
                 .then((response) => {
                     console.log("send");
                 })
